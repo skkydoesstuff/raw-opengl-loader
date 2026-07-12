069a20d809db0cd6bcee960a3a7ac9aa10f9742a
skky <petesky26@gmail.com>
2026-07-12 07:29:55 -0400

    removed some unused imports

--- src/core/file_utils/file_utils.c
+++ src/core/file_utils/file_utils.c
@@ -1,66 +1,76 @@
 #include "core/file_utils/file_utils.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <windows.h>
 
 #include "logging.h"
 
-char* get_executable_path() {
-  char* exePath = malloc(MAX_PATH);
-  if (!exePath) {
+char* get_executable_path()
+{
+  char* exe_path = malloc(MAX_PATH);
+
+  if (!exe_path) {
     print("Memory allocation failed");
+    return NULL;
   }
 
-  DWORD result = GetModuleFileName(NULL, exePath, MAX_PATH);
+  DWORD result = GetModuleFileNameA(NULL, exe_path, MAX_PATH);
+
   if (result == 0 || result == MAX_PATH) {
     print("Failed to get executable path");
-    free(exePath);
+    free(exe_path);
+    return NULL;
   }
 
-  char* last_backslash = strrchr(exePath, '\\');
+  char* last_backslash = strrchr(exe_path, '\\');
+
   if (last_backslash) {
     *last_backslash = '\0';
   }
 
-  return exePath;
+  return exe_path;
 }
 
-char* read_file(const char* path) {
-  HANDLE file = CreateFileA(
-    path,
-    GENERIC_READ,
-    FILE_SHARE_READ,
-    NULL,
-    OPEN_EXISTING,
-    FILE_ATTRIBUTE_NORMAL,
-    NULL
-  );
 
-  if (file == INVALID_HANDLE_VALUE) {
+char* read_file(const char* path)
+{
+  FILE* file = fopen(path, "rb");
+
+  if (!file) {
     return NULL;
   }
 
-  LARGE_INTEGER size;
-  if (!GetFileSizeEx(file, &size) || size.QuadPart <= 0) {
-    CloseHandle(file);
+  fseek(file, 0, SEEK_END);
+
+  long size = ftell(file);
+
+  if (size <= 0) {
+    fclose(file);
     return NULL;
   }
 
-  char* buffer = malloc((size_t)size.QuadPart + 1);
+  rewind(file);
+
+  char* buffer = malloc((size_t)size + 1);
+
   if (!buffer) {
-    CloseHandle(file);
+    fclose(file);
     return NULL;
   }
 
-  DWORD bytes_read;
-  if (!ReadFile(file, buffer, (DWORD)size.QuadPart, &bytes_read, NULL) ||
-    bytes_read != (DWORD)size.QuadPart) {
+  size_t bytes_read = fread(buffer, 1, (size_t)size, file);
+
+  if (bytes_read != (size_t)size) {
     free(buffer);
-    CloseHandle(file);
+    fclose(file);
     return NULL;
   }
 
-  buffer[size.QuadPart] = '\0';
+  buffer[size] = '\0';
 
-  CloseHandle(file);
+  fclose(file);
+
   return buffer;
 }
--- src/core/opengl_loader/opengl_function_ptrs.h
+++ src/core/opengl_loader/opengl_function_ptrs.h
@@ -1,48 +1,45 @@
 #pragma once
 
-#include <stdint.h>
-#include <windows.h>
-
 #include "core/opengl_loader/opengl_types.h"
 
 // general opengl functions
-typedef void (APIENTRY*glViewportProc)(GLint x, GLint y, GLsizei width, GLsizei height);
-typedef void (APIENTRY*glClearColorProc)(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
-typedef void (APIENTRY*glClearProc)(GLbitfield mask);
-typedef void (APIENTRY*glEnableProc)(GLenum cap);
-typedef void (APIENTRY*glDisableProc)(GLenum cap);
-typedef const GLubyte* (APIENTRY*glGetStringProc)(GLenum name);
+typedef void (__stdcall*glViewportProc)(GLint x, GLint y, GLsizei width, GLsizei height);
+typedef void (__stdcall*glClearColorProc)(GLfloat red, GLfloat green, GLfloat blue, GLfloat alpha);
+typedef void (__stdcall*glClearProc)(GLbitfield mask);
+typedef void (__stdcall*glEnableProc)(GLenum cap);
+typedef void (__stdcall*glDisableProc)(GLenum cap);
+typedef const GLubyte* (__stdcall*glGetStringProc)(GLenum name);
 
 // vertex array functions
-typedef void (APIENTRY*glGenVertexArraysProc)(GLsizei n, GLuint* arrays);
-typedef void (APIENTRY*glBindVertexArrayProc)(GLuint array);
-typedef void (APIENTRY*glDeleteVertexArraysProc)(GLsizei n, GLuint* arrays);
-typedef void (APIENTRY*glVertexAttribPointerProc)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* pointer);
-typedef void (APIENTRY*glEnableVertexAttribArrayProc)(GLuint index);
+typedef void (__stdcall*glGenVertexArraysProc)(GLsizei n, GLuint* arrays);
+typedef void (__stdcall*glBindVertexArrayProc)(GLuint array);
+typedef void (__stdcall*glDeleteVertexArraysProc)(GLsizei n, GLuint* arrays);
+typedef void (__stdcall*glVertexAttribPointerProc)(GLuint index, GLint size, GLenum type, GLboolean normalized, GLsizei stride, const void* pointer);
+typedef void (__stdcall*glEnableVertexAttribArrayProc)(GLuint index);
 
 // buffer functions
 typedef void (__stdcall*glGenBuffersProc)(GLsizei n, GLuint* buffers);
-typedef void (APIENTRY*glBindBufferProc)(GLenum target, GLuint buffer);
-typedef void (APIENTRY*glBufferDataProc)(GLenum target, GLsizeiptr size, const void* data, GLenum usage);
-typedef void (APIENTRY*glDeleteBuffersProc)(GLsizei n, GLuint* buffers);
+typedef void (__stdcall*glBindBufferProc)(GLenum target, GLuint buffer);
+typedef void (__stdcall*glBufferDataProc)(GLenum target, GLsizeiptr size, const void* data, GLenum usage);
+typedef void (__stdcall*glDeleteBuffersProc)(GLsizei n, GLuint* buffers);
 
 // drawing functions
-typedef void (APIENTRY*glDrawArraysProc)(GLenum mode, GLint first, GLsizei count);
-typedef void (APIENTRY*glDrawElementsProc)(GLenum mode, GLsizei count, GLenum type, const void* indices);
+typedef void (__stdcall*glDrawArraysProc)(GLenum mode, GLint first, GLsizei count);
+typedef void (__stdcall*glDrawElementsProc)(GLenum mode, GLsizei count, GLenum type, const void* indices);
 
 // shader functions
-typedef GLuint (APIENTRY*glCreateShaderProc)(GLenum shaderType);
-typedef GLuint (APIENTRY*glDeleteShaderProc)(GLuint shader);
-typedef void (APIENTRY*glShaderSourceProc)(GLuint shader, GLsizei count, const GLchar** string, const GLint* length);
-typedef void (APIENTRY*glCompileShaderProc)(GLuint shader);
-typedef GLuint (APIENTRY*glCreateProgramProc)();
-typedef GLuint (APIENTRY*glDeleteProgramProc)(GLuint program);
-typedef void (APIENTRY*glAttachShaderProc)(GLuint program, GLuint shader);
-typedef void (APIENTRY*glDetachShaderProc)(GLuint program, GLuint shader);
-typedef void (APIENTRY*glLinkProgramProc)(GLuint program);
-typedef void (APIENTRY*glUseProgramProc)(GLuint program);
+typedef GLuint (__stdcall*glCreateShaderProc)(GLenum shaderType);
+typedef GLuint (__stdcall*glDeleteShaderProc)(GLuint shader);
+typedef void (__stdcall*glShaderSourceProc)(GLuint shader, GLsizei count, const GLchar** string, const GLint* length);
+typedef void (__stdcall*glCompileShaderProc)(GLuint shader);
+typedef GLuint (__stdcall*glCreateProgramProc)();
+typedef GLuint (__stdcall*glDeleteProgramProc)(GLuint program);
+typedef void (__stdcall*glAttachShaderProc)(GLuint program, GLuint shader);
+typedef void (__stdcall*glDetachShaderProc)(GLuint program, GLuint shader);
+typedef void (__stdcall*glLinkProgramProc)(GLuint program);
+typedef void (__stdcall*glUseProgramProc)(GLuint program);
 
 // uniform functions
-typedef GLint (APIENTRY*glGetUniformLocationProc)(GLuint program, const GLchar* name);
+typedef GLint (__stdcall*glGetUniformLocationProc)(GLuint program, const GLchar* name);
 
-typedef void (APIENTRY*glUniformMatrix4fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* val);
+typedef void (__stdcall*glUniformMatrix4fvProc)(GLint location, GLsizei count, GLboolean transpose, const GLfloat* val);
